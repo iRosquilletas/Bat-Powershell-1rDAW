7c883368f726bed58f925289ae0f034669abcee4
iRosquilletas <samyelestudio@gmail.com>
2023-12-20 14:52:09 +0100

    new

---
+++ powershell-ISE/ejercicios-A/bonoloto-me.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host " --- Bonoloto ---- "
+$a = @()
+
+for ($i = 0; $i -lt 6;){
+    $random = Get-Random -Minimum 1 -Maximum 49
+    if ($a -notcontains $random){
+    $a += $random
+    $i++ 
+    }
+}
+
+[array]::Sort($a)
+Write-Host $a
+
+$a = $a | Sort-Object -Descending
+Write-Host $a
---
+++ powershell-ISE/ejercicios-A/mostrarmensaje-me.ps1
@@ -0,0 +1,7 @@
+﻿$a = Read-Host "Dime que mensaje quieres mostrar: "
+[int]$b = Read-Host "Dime cuantas veces: "
+
+for ($i = $b; $i -gt 0; $i--){
+    Write-Host $i $a 
+
+}
